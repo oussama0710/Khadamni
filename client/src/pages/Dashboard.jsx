5e0e8d817981bd851b77b0f029a531e71a4c62fd
Ennosukeblade <mouadhjenouiz@gmail.com>
2023-04-03 17:00:33 +0100

    Show services and dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Services from "../components/Services";
 import AddService from "../components/AddService";
+import Services from "../components/Services";
 
 const Dashboard = () => {
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/services" element={<Services />} />
-                    <Route path="/services/add" element={<AddService />} />
-                </Routes>
-            </BrowserRouter>
+            <AddService />
+            <Services />
         </div>
     );
 };
